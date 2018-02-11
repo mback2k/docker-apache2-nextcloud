00418aecdf006a912d2758fde382c94d784a5843
Marc Hoersken <info@marc-hoersken.de>
2018-02-11 16:39:03 +0100

    Fix execution of su command without default shell and line breaks

--- docker-entrypoint.d/01-install-nextcloud.sh
+++ docker-entrypoint.d/01-install-nextcloud.sh
@@ -9,10 +9,10 @@ if [ -n "${NEXTCLOUD_DATABASE_USER}" ]; then
 if [ -n "${NEXTCLOUD_DATABASE_PASS}" ]; then
 if [ -n "${NEXTCLOUD_ADMIN_USER}" ]; then
 if [ -n "${NEXTCLOUD_ADMIN_PASS}" ]; then
-    su -c 'cd /var/www/nextcloud; php occ maintenance:install
-        --data-dir "${NEXTCLOUD_DATA_DIR}" --database "${NEXTCLOUD_DATABASE}"
-        --database-host "${NEXTCLOUD_DATABASE_HOST}" --database-name "${NEXTCLOUD_DATABASE_NAME}"
-        --database-user "${NEXTCLOUD_DATABASE_USER}" --database-pass "${NEXTCLOUD_DATABASE_PASS}"
+    su -s /bin/sh -c 'cd /var/www/nextcloud; php occ maintenance:install \
+        --data-dir "${NEXTCLOUD_DATA_DIR}" --database "${NEXTCLOUD_DATABASE}" \
+        --database-host "${NEXTCLOUD_DATABASE_HOST}" --database-name "${NEXTCLOUD_DATABASE_NAME}" \
+        --database-user "${NEXTCLOUD_DATABASE_USER}" --database-pass "${NEXTCLOUD_DATABASE_PASS}" \
         --admin-user "${NEXTCLOUD_ADMIN_USER}" --admin-pass "${NEXTCLOUD_ADMIN_PASS}"' www-data
 fi
 fi
