eadfa5d04979899d5b2d4d773e411e34727dfe6c
Marc Hoersken <info@marc-hoersken.de>
2020-03-20 23:46:42 +0100

    Add overwriting of host and protocol via environment variable

--- docker-entrypoint.d/16-config-revproxy.sh
+++ docker-entrypoint.d/16-config-revproxy.sh
@@ -6,4 +6,14 @@ if [ -n "${NEXTCLOUD_TRUSTED_PROXY}" ]; then
         trusted_proxies 1 --value="${NEXTCLOUD_TRUSTED_PROXY}"'
 fi
 
+if [ -n "${NEXTCLOUD_OVERWRITE_HOST}" ]; then
+    runuser -u www-data -- /bin/sh -c 'cd /var/www/nextcloud; php occ config:system:set \
+        overwritehost --value="${NEXTCLOUD_OVERWRITE_HOST}"'
+fi
+
+if [ -n "${NEXTCLOUD_OVERWRITE_PROTOCOL}" ]; then
+    runuser -u www-data -- /bin/sh -c 'cd /var/www/nextcloud; php occ config:system:set \
+        overwriteprotocol --value="${NEXTCLOUD_OVERWRITE_PROTOCOL}"'
+fi
+
 exit 0
