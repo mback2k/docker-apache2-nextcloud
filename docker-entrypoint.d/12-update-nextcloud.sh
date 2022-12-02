c9815bde9094055538faf4d05b7d1349cfaaecd3
Marc Hoersken <info@marc-hoersken.de>
2022-12-02 22:26:16 +0100

    Add occ db:add-missing-indices to upgrade script

--- docker-entrypoint.d/12-update-nextcloud.sh
+++ docker-entrypoint.d/12-update-nextcloud.sh
@@ -3,4 +3,6 @@ set -e
 
 runuser -u www-data -- /bin/sh -c 'cd /var/www/nextcloud; php occ upgrade'
 
+runuser -u www-data -- /bin/sh -c 'cd /var/www/nextcloud; php occ db:add-missing-indices'
+
 exit 0
